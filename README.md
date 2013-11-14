6ba35ca625e41d77901f47b44eeaa7c5927e2f16
Ambrose Little <ambrogio@gmail.com>
2013-11-14 13:15:39 -0500

    Initial setup for header and paragraph.

--- README.md
+++ README.md
@@ -2,3 +2,16 @@ ig-pkg-html
 ===========
 
 Base HTML package for Ignite UI Web Designer.
+
+# To get set up using this with the Web Designer project..
+
+1. Go to web-designer, master branch. Pull latest.
+2. It should have this repo as a git submodule; run `git submodule init` and `git submodule update` ([see here for more info](http://joncairns.com/2011/10/how-to-use-git-submodules/#cloning)).
+3. Go to `cd <web-designer root>/modules/html` (where the repo should be added as a submodule).
+4. Run `npm install`; this should get all package dependencies.
+5. Go to `cd ../..` to get back to parent repo of web-designer and run `grunt build`.  This should build this package and then grab its dist output and put it into the lib/packages dir, so it will be available along with the other packages currently under src/packages (both are mounted when running grunt server).
+6. Run `grunt server` from the web-designer repo dir, and it should run, loading both packages in src/packages as well as any in lib/packages.  
+
+You should see the html package in the IDE. You can work on both the web-designer and ig-pkg-html repos independently. Git treats submodules as their own repos. 
+
+ig-pkg-html is just on master for now. 
--- src/plugins/_default-component.js
+++ src/plugins/_default-component.js
@@ -1,112 +1,52 @@
 define(function (require, exports, module) {
   var ComponentPlugin = require("ide-component-plugin");
-  var BootstrapComponent = BootstrapComponent || ComponentPlugin.extend({
+  var HtmlComponent = HtmlComponent || ComponentPlugin.extend({
     init: function (options) {
       this._super(options);
-      // bind ti dblclick event in order to set content editables
-      var ide = this.settings.ide;
       return this;
-    },
-    _getDefaultColSpan: function(descriptor) {
-      var colComponent = this.settings.packageInfo.components[descriptor.type];
-      if (!colComponent) {
-        throw "Could not find column component for bootstrap package.";
-      }
-      if (!colComponent.properties) {
-        // we are using the component before it has been dropped/fully initialized, so we need to load its info
-        colComponent.loadInfo();
-      }
-      return colComponent.properties["columnSpan"].defaultValue;
     },
     getMarkup: function (descriptor) {
       var code = "", i;
-      //return "<div id=\"" + descriptor.id + "\"></div>";
-      if (descriptor.type === "row") {
-        for (i = 0; i < 3; i++) {
-          code += this.getMarkup({type: "column", colSpan: 4});
-        }
-        return "<div data-droppablechild=\"false\" data-hasdroppables=\"true\" class=\"row\">" + code + "</div>";
-      } else if (descriptor.type === "column") {
-        var colSpan = descriptor.colSpan;
-        if (typeof (colSpan) === "undefined") {
-          // get the default one
-          colSpan = this._getDefaultColSpan(descriptor);
-        }
-        return "<div data-droppablechild=\"true\" class=\"col-md-" + colSpan + "\"><h3>Span " + colSpan + "</h3><p>Contents</p></div>";
+      switch (descriptor.type) {
+        case "header":
+          return "<h1>Lorem Ipsum Dolor Sit Amet</h1>"
+          break;
+        case "paragraph":
+          return "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac blandit erat. Curabitur velit libero, lacinia sit amet dolor a, tincidunt varius nisi. Integer ac magna bibendum, dignissim quam sed, commodo mauris. Aenean consequat ullamcorper felis ut sagittis. Quisque at magna a purus egestas suscipit. In vulputate tincidunt arcu non pulvinar. Pellentesque vitae leo et justo fringilla adipiscing sit amet eget lacus.</p>";
+          break;
+        default:
+          console.log("Unknown HTML element added.");
+          return "";
+          break;
       }
     },
-    getCodeEditorMarkupSnippet: function (descriptor) {
-      if (descriptor.type === "column") {
-        var colSpan = descriptor.colSpan, indentStr = "\t\t";
-        if (descriptor.extraIndent) {
-          for (var i = 0; i < descriptor.extraIndent; i++) {
-            indentStr += "\t";
-          }
+    _getIndentTabs: function(descriptor) {
+      var extraIndentStr = "", i = 0;
+      if (descriptor.extraIndent) {
+        for (i = 0; i < descriptor.extraIndent; i++) {
+          extraIndentStr += "\t";
         }
-        if (typeof (colSpan) === "undefined") {
-          colSpan = this._getDefaultColSpan(descriptor);
-        }
-        return {
-          codeString: indentStr + "<div class=\"col-md-" + colSpan + "\">\n" +
-            indentStr + "\t<h3>Span " + colSpan + "</h3>\n" +
-            indentStr + "\t<p>Contents</p>\n" +
-            indentStr + "</div>\n",
-          lineCount: 4
-        };
-      } else {
-        return {
-          codeString: "\t\t<div class=\"row\">\n" +
-            "\t\t\t<div class=\"col-md-4\">\n" + 
-            "\t\t\t\t<h3>Span 4</h3>\n" +
-            "\t\t\t\t<p>Contents</p>\n" +
-            "\t\t\t</div>\n" +
-            "\t\t\t<div class=\"col-md-4\">\n" +
-            "\t\t\t\t<h3>Span 4</h3>\n" +
-            "\t\t\t\t<p>Contents</p>\n" +
-            "\t\t\t</div>\n" +
-            "\t\t\t<div class=\"col-md-4\">\n" +
-            "\t\t\t\t<h3>Span 4</h3>\n" +
-            "\t\t\t\t<p>Contents</p>\n" +
-            "\t\t\t</div>\n" +
-            "\t\t</div>\n",
-          lineCount: 14,
-          // contains offsets from parent only only
-          extraMarkers: [
-            {
-              rowOffset: 1,
-              colOffset: 0,
-              rowCount: 4,
-              colCount: 0
-            },
-            {
-              rowOffset: 5,
-              colOffset: 0,
-              rowCount: 4, 
-              colCount: 0
-            },
-            {
-              rowOffset: 9,
-              colOffset: 0,
-              rowCount: 4,
-              colCount: 0
-            }
-          ]
-        };
       }
+      return extraIndentStr;
+    },
+    getCodeEditorMarkupSnippet: function (descriptor) {
+      var snippet = "\t\t" + this._getIndentTabs(descriptor) + this.getMarkup(descriptor) + "\n";
+      return {codeString: snippet, lineCount: 1};
     },
     isContainer: function (descriptor) {
       if (typeof (descriptor) === "undefined" || descriptor === null) {
         return false;
       }
-      //returns true for both  columns and rows
-      if (descriptor.type === "row" || descriptor.type === "column") {
-        return true;
+      switch (descriptor.type) {
+        case "header":
+        case "paragraph":
+          return true;
+          break;
+        default:
+          console.log("Unknown HTML element added.");
+          return false;
+          break;
       }
-      // the descriptor can also be a DOM element
-      if (descriptor.nodeName && $(descriptor).attr("data-container") === "true") {
-        return true;
-      }
-      return false;
     },
     isDroppableChild: function (descriptor) {
       if (typeof (descriptor) === "undefined" || descriptor === null) {
@@ -115,11 +55,7 @@ define(function (require, exports, module) {
       if (descriptor.nodeName && $(descriptor).attr("data-droppablechild") === "true") {
         return true;
       } else {
-        if (descriptor.type === "row") {
-          return false;
-        } else if (descriptor.type === "column") {
-          return true;
-        }
+        return this.isContainer(descriptor);
       }
       return false;
     },
@@ -140,5 +76,5 @@ define(function (require, exports, module) {
       return $();
     }
   });
-  return BootstrapComponent;
+  return HtmlComponent;
 });

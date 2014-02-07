96d5da747f9aa01e74e0aea2e8de143a5a68ad39
Angel Todorov <attodorov@gmail.com>
2014-02-07 16:10:03 +0200

    working on toolbox filtering

--- src/plugins/_default-component.js
+++ src/plugins/_default-component.js
@@ -7,8 +7,8 @@ define(function (require, exports, module) {
     },
     _blockOfText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac blandit erat. Curabitur velit libero, lacinia sit amet dolor a, tincidunt varius nisi. Integer ac magna bibendum, dignissim quam sed, commodo mauris. Aenean consequat ullamcorper felis ut sagittis. Quisque at magna a purus egestas suscipit. In vulputate tincidunt arcu non pulvinar. Pellentesque vitae leo et justo fringilla adipiscing sit amet eget lacus.",
     _lineOfText: "Lorem Ipsum Dolor Sit Amet",
-    _getTextElementMarkup: function(tagName, forCode, singleLine) {
-      return "<" + tagName + this._getContentEditable(forCode) + ">" + (singleLine ? this._lineOfText : this._blockOfText) +  "</" + tagName + ">"; 
+    _getTextElementMarkup: function(tagName, forCode, singleLine, id) {
+      return "<" + tagName + this._getContentEditable(forCode) + " id=\"" + id + "\">" + (singleLine ? this._lineOfText : this._blockOfText) +  "</" + tagName + ">"; 
     },
     _getContentEditable: function(forCode) {
       return (forCode ? "" : " contenteditable=\"true\" spellcheck=\"false\"");
@@ -16,23 +16,23 @@ define(function (require, exports, module) {
     getMarkup: function (descriptor, forCode) {
       switch (descriptor.type) {
         case "heading":
-          return this._getTextElementMarkup("h1", forCode, true);
+          return this._getTextElementMarkup("h1", forCode, true, descriptor.id);
         case "paragraph":
-          return this._getTextElementMarkup("p", forCode);
+          return this._getTextElementMarkup("p", forCode, false, descriptor.id);
         case "link":
-          return this._getTextElementMarkup("a", forCode, true);
+          return this._getTextElementMarkup("a", forCode, true, descriptor.id);
         case "list":
-          return "<ul" + this._getContentEditable(forCode) + "><li>Item 1</li><li>Item 2</li></ul>";
+          return "<ul" + this._getContentEditable(forCode) + " id=\"" + descriptor.id + "\"><li>Item 1</li><li>Item 2</li></ul>";
         case "container":
           if (forCode) {
-            return "<div></div>";
+            return "<div id=\"" + descriptor.id + "\"></div>";
           } else {
-            return "<div class=\"containerElement\"" + this._getContentEditable(false) + "></div>";
+            return "<div class=\"containerElement\"" + this._getContentEditable(false) + " id=\"" + descriptor.id + "\"></div>";
           }
         case "button":
-          return "<button" + this._getContentEditable(forCode) + ">Button</button>";
+          return "<button" + this._getContentEditable(forCode) + " id=\"" + descriptor.id + "\">Button</button>";
         case "input":
-          return "<input" + this._getContentEditable(forCode) + "/>";
+          return "<input" + this._getContentEditable(forCode) + " id=\"" + descriptor.id + "\"/>";
         default:
           console.log("Unknown HTML element added: " + descriptor.type);
           return "";
